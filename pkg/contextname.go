711b85f8d37aeeab23adca99cc8c5b03aca67669
Fredrik Sommar <fred.sommar+github@gmail.com>
2021-12-20 15:27:29 +0100

    Export named type i/o interface

--- pkg/contextname.go
+++ pkg/contextname.go
@@ -20,9 +20,9 @@ type ContextNameFactory interface {
 	For(string, Cluster) (string, error)
 }
 
-type contextNameFactory struct {
+type TemplateFactory struct {
 	t      *template.Template
-	format string
+	Format string
 }
 
 type data struct {
@@ -30,7 +30,7 @@ type data struct {
 	Cluster Cluster
 }
 
-func (c contextNameFactory) For(project string, cluster Cluster) (string, error) {
+func (c TemplateFactory) For(project string, cluster Cluster) (string, error) {
 	contextName := strings.Builder{}
 	if err := c.t.Execute(&contextName, &data{
 		Project: project,
@@ -38,27 +38,30 @@ func (c contextNameFactory) For(project string, cluster Cluster) (string, error)
 	}); err != nil {
 		return "", err
 	}
+
 	return contextName.String(), nil
 }
 
-var _ ContextNameFactory = (*contextNameFactory)(nil)
+var _ ContextNameFactory = (*TemplateFactory)(nil)
 
 // NewContextNameFactory returns a new ContextNameFactory, preferring the format string in `$KUBECTL_GKE_CONTEXT_FORMAT`
 // over the provided string.
-func NewContextNameFactory(fallback string) (ContextNameFactory, error) {
+func NewContextNameFactory(fallbackFormat string) (*TemplateFactory, error) {
 	var format string
 	if env, exists := os.LookupEnv("KUBECTL_GKE_CONTEXT_FORMAT"); exists {
 		format = env
-	} else if fallback == "" {
+	} else if fallbackFormat == "" {
 		format = GcloudDefaultFormat
 	} else {
-		format = fallback
+		format = fallbackFormat
 	}
+
 	t, err := template.New("context-name").Parse(format)
 	if err != nil {
 		return nil, err
 	}
-	return contextNameFactory{t: t}, nil
+
+	return &TemplateFactory{t: t, Format: format}, nil
 }
 
 // DefaultContextName returns the default context name used by the gcloud get-credentials command.
